f5014ae36c126d740d2a16e5aee86a2dbbedf295
madzwo <madzwo@iu.edu>
2024-03-27 23:06:54 -0400

    spawner

---
+++ Object Placement Prototype/Assets/Spawner.cs
@@ -0,0 +1,29 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawner : MonoBehaviour
+{
+    public Transform spawnPoint;
+    public GameObject enemyPrefab;
+
+    public float spawnRate;
+    private float timeTillSpawn;
+    public float spawnRateIncreaseRate;
+
+    void Start()
+    {
+        timeTillSpawn = spawnRate;
+    }
+
+    void Update()
+    {
+        if(timeTillSpawn <= 0)
+        {
+            Instantiate(enemyPrefab, spawnPoint.position, spawnPoint.rotation);
+            timeTillSpawn = spawnRate;
+            spawnRate -= spawnRateIncreaseRate;
+        }
+        timeTillSpawn -= Time.deltaTime;
+    }
+}
